10ecf1fe8b11496d5460e3a00a9a5282555e9a53
rahulthakor2295 <rahulthakor8609@gmail.com>
2022-09-21 16:10:10 +0530

    chard chgand

--- lib/Input_page.dart
+++ lib/Input_page.dart
@@ -1,35 +1,80 @@
-// import 'package:flutter/material.dart';
-// class InputPage extends StatefulWidget {
-//   const InputPage({super.key, required this.title});
+import 'package:flutter/material.dart';
 
-//   final String title;
+class InputPage extends StatefulWidget {
+  const InputPage({super.key, required this.title});
 
-//   @override
-//   State<InputPage> createState() => _InputPageState();
-// }
+  final String title;
 
-// class _InputPageState extends State<InputPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         home: Scaffold(
-//       backgroundColor: Colors.black12,
-//       appBar: AppBar(
-//         backgroundColor: Colors.black12,
-//         title: Text(widget.title),
-//         centerTitle: true,
-//       ),
-//       body: Column(
-//         children: [
-//           SizedBox(
-//             child: Theme(
-//               data: ThemeData(splashColor: Colors.red),
-//               child: ElevatedButton(
-//                   onPressed: () {}, child: Text("CALCULATE YOUR BMI")),
-//             ),
-//           ),
-//         ],
-//       ),
-//     ),);
-//   }
-// }
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black12,
+        appBar: AppBar(
+          backgroundColor: Colors.black12,
+          title: Text(widget.title),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: CardResuable(
+                      colur: Color(0xFF0C1234),
+                    ),
+                  ),
+                  Expanded(
+                    child: CardResuable(
+                      colur: Color(0xFF0C1234),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: CardResuable(
+                colur: Color(0xFF0C1234),
+              ),
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: CardResuable(
+                    colur: Colors.blue,
+                  ),
+                ),
+                Expanded(
+                  child: CardResuable(
+                    colur: Color(0xFF0C1234),
+                  ),
+                ),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardResuable extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
+  const CardResuable({required this.colur});
+  final Color colur;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: colur, borderRadius: BorderRadius.circular(10.0)),
+      margin: EdgeInsets.all(15.0),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi/Input_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,40 +18,5 @@ class BmiCalculator extends StatelessWidget {
           buttonColor: Colors.red),
       home: const InputPage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<InputPage> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.black12,
-      appBar: AppBar(
-        backgroundColor: Colors.black12,
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            child: Theme(
-              data: ThemeData(splashColor: Colors.red),
-              child: ElevatedButton(
-                  onPressed: () {}, child: Text("CALCULATE YOUR BMI")),
-            ),
-          ),
-        ],
-      ),
-    ));
   }
 }
