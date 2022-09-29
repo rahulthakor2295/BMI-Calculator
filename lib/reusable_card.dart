80b77d6ef4e284c68c69b9e8fd6920aa13fd613e
rahulthakor2295 <rahulthakor8609@gmail.com>
2022-09-29 16:00:50 +0530

    use Function as paramerter in costrustor in input page

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 
 class CardResuable extends StatelessWidget {
   // ignore: use_key_in_widget_constructors, non_constant_identifier_names
-  const CardResuable({required this.colur, this.ChardChild});
+  const CardResuable({required this.colur, this.ChardChild, this.onPress});
   final Color colur;
   final Widget? ChardChild;
+  final Function()? onPress;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ChardChild,
-      decoration: BoxDecoration(
-          color: colur, borderRadius: BorderRadius.circular(10.0)),
-      margin: EdgeInsets.all(15.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: ChardChild,
+        decoration: BoxDecoration(
+            color: colur, borderRadius: BorderRadius.circular(10.0)),
+        margin: EdgeInsets.all(15.0),
+      ),
     );
   }
 }
